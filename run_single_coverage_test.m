59d2bd4f62e88c0276ee915d73ea78daf365fc00
ElbergDenStore <jacob@askildrup.net>
2026-03-05 13:46:48 +0100

    added run single test

---
+++ run_single_coverage_test.m
@@ -0,0 +1,36 @@
+clear all; close all; clc;
+
+Cfg.StartTime  = datetime('1-Jun-2025 00:00:00', 'TimeZone', 'UTC');
+Cfg.StopTime   = datetime('1-Jun-2025 23:59:59', 'TimeZone', 'UTC');
+Cfg.SampleTime = 20; % seconds
+
+Cfg.Lat_vec = linspace(50, 85, 6); 
+% Cfg.lat_vec = linspace(50, 85, 8); 
+Cfg.Lon_vec = linspace(-60, 30, 3);
+
+
+
+% Downlink Link Budget Config
+Cfg.DL.Direction = "DL";
+Cfg.DL.B         = 2e6;     
+Cfg.DL.f         = 26e9;    
+Cfg.DL.P_tx      = 20; 
+Cfg.DL.G_tx      = 36; 
+Cfg.DL.Tx_type   = "array";      
+Cfg.DL.G_rx      = 38.3;      
+Cfg.DL.Rx_type   = "array";      
+Cfg.DL.NF        = 5;
+Cfg.DL.EIRP_dBm  = Cfg.DL.P_tx + Cfg.DL.G_tx;
+
+% Constellation
+Cfg.Min_elevation_UE = 20;
+Cfg.Orbit_height = 932e3;
+Cfg.Num_planes     = 5;  
+Cfg.Sats_per_plane = 14;
+Cfg.Inclination    = 88; 
+Cfg.Phasing = Cfg.Num_planes / 2;
+Cfg.WalkerStar     = true;  
+
+plot_results = true;
+metrics = coverage_simulator_function(Cfg, plot_results);
+fprintf("Worst Coverage percentage" + metrics.worst_coverage_percent);
